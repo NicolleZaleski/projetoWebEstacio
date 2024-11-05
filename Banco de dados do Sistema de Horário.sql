3ff51c8163e2cc1211e18432ddf4ab98899bbb39
NicolleZaleski <nicolle.m.zaleski@gmail.com>
2024-11-05 18:12:50 -0300

    Banco de dados
    Alterei o banco de dados das aulas cadastradas e adicionei o elemento "enum" nas chaves "ensino","diaSemana" e "periodo".

--- Banco de dados do Sistema de Horário.sql
+++ Banco de dados do Sistema de Horário.sql
@@ -5,11 +5,13 @@ create table Aulas(
 id_aula int auto_increment primary key,
 curso varchar(100) not null,
 nomeAula varchar(200) not null,
+ensino enum('presencial','semipresencial') not null,
 professor varchar(100) not null,
-diaSemana varchar(30) not null,
-periodo varchar(15) not null,
+diaSemana enum('segunda','terça','quarta','quinta','sexta','sábado') not null,
+periodo enum('matutino','vespertino','noturno') not null,
 horario time not null,
 bloco varchar(20) not null,
 andar varchar(20) not null,
 sala varchar(20) not null
 );
+
