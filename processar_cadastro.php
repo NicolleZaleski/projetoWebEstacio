ab955fa68a4c237e09464819f08088ac45c1bfc9
NicolleZaleski <nicolle.m.zaleski@gmail.com>
2024-11-04 22:42:48 -0300

    Processar cadastro
    Fiz o arquivo de processar as informações adquiridas na página HTML e enviando-as para o banco de dados

--- processar_cadastro.php
+++ processar_cadastro.php
@@ -1,4 +1,28 @@
 <?php
-    
+    $host = 'localhost';
+    $db = 'sistemaHorarios';
+    $user = 'root';
+    $password = 'dev321'
+    $conn = new mysqli($host,$db,$user,$password);
 
+    $curso = $_POST['curso'];
+    $nomeAula = $_POST['nomeAula'];
+    $ensino = $_POST['ensino'];
+    $professor = $_POST['professor'];
+    $diaSemana = $_POST['diaSemana'];
+    $periodo = $_POST['periodo'];
+    $horario = $_POST['horario'];
+    $bloco = $_POST['bloco'];
+    $andar = $_POST['andar'];
+    $sala = $_POST['sala'];
+    $sql = "insert into Aulas(curso,nomeAula,ensino,professor,diaSemana,periodo,horario,bloco,andar,sala) values('$curso','$nomeAula','$ensino','$professor','$diaSemana','$periodo','$horario','$bloco','$andar','$sala')";
+
+    if ($conn->query($sql)=== TRUE){
+        echo "Aula cadastrada com sucesso.";
+    }
+    else ($conn->connect_error){
+        echo"Erro! Aula não cadastrada.";
+    }
+
+    $conn->close();
 ?>
